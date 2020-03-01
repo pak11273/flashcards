c4773a27d2f7ba5b398d50e176b30ec07bd853c3
pak11273 <pak11273@gmail.com>
2020-02-29 23:44:26 -0600

    adds flip feature to cards with no pics

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
 /* eslint no-unused-vars:0, no-dupe-keys:0 */
 import React from "react"
 import "./App.scss"
-
-let langData = [
-  { word: "color", translation: "색깔", meta: "" },
-  { word: "white", translation: "흰색", meta: "" },
-  { word: "Black", translation: "or dark	검은색", meta: "" },
-  { word: "Brown", translation: "갈색", meta: "" },
-  { word: "Gray", translation: "회색", meta: "" },
-  { word: "Green", translation: "녹색", meta: "" },
-  { word: "Black", translation: "and white	흑백", meta: "" },
-  { word: "White", translation: "백색", meta: "" },
-  { word: "Yellow", translation: "노란색", meta: "" },
-  { word: "deep-red crimson", translation: "빨간색", meta: "" },
-  { word: "rainbow", translation: "무지개", meta: "" },
-  { word: "Pink", translation: "color	분홍색", meta: "" },
-  { word: "Blue", translation: "파란색", meta: "" },
-  { word: "Green", translation: "초록색", meta: "" },
-  { word: "Purple", translation: "보라색", meta: "" },
-  { word: "Lime green", translation: "연두색", meta: "" },
-  { word: "Black", translation: "까만색", meta: "" },
-  { word: "White", translation: "하얀색", meta: "" },
-  { word: "skin", translation: "color	살색", meta: "" },
-  { word: "color", translation: "색", meta: "" },
-  { word: "color", translation: "색상", meta: "" },
-  { word: "black", translation: "검정색", meta: "" },
-  { word: "orange", translation: "주황색", meta: "" },
-  { word: "deep blue", translation: "남색", meta: "" },
-  { word: "light blue", translation: "하늘색", meta: "" },
-  { word: "silver", translation: "은색", meta: "" },
-  { word: "gold", translation: "금색", meta: "" },
-  { word: "pink", translation: "핑크색", meta: "" },
-  { word: "To be green", translation: "푸르다", meta: "" },
-  { word: "fair", translation: "희다", meta: "" },
-  { word: "White", translation: "하얗다", meta: "" },
-  { word: "Be red scarlet", translation: "붉다", meta: "" },
-  { word: "deep-red crimson", translation: "빨갛다", meta: "" },
-  { word: "Be", translation: "yellow	노랗다", meta: "" },
-  { word: "Black", translation: "까맣다", meta: "" },
-  { word: "To be blue", translation: "파랗다", meta: "" },
-  { word: "To be pitch-black", translation: "캄캄하다", meta: "" }
-]
+import langData from "./data/colors"
 
 async function fetchImg(word) {
   let url = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_KEY}&q=${word}&image_type=photo&pretty=true`
@@ -52,6 +13,7 @@ const randomize = () => {}
 
 class App extends React.PureComponent {
   state = {
+    recallRate: 100,
     langData,
     counter: 0,
     picFlipped: false,
@@ -69,45 +31,73 @@ class App extends React.PureComponent {
     })
   }
 
+  handleRecall100 = () => {
+    this.nextWord()
+  }
+
+  handleRecall50 = () => {
+    let review = this.state.langData[this.state.counter]
+    console.log("review: ", review)
+    let langData = this.state.langData.concat(review)
+    this.setState(
+      {
+        langData
+      },
+      () => console.log(this.state)
+    )
+    this.nextWord()
+  }
+
   reveal = () => {
     this.setState({
-      imgURL: null
+      imgURL: null,
+      picFlipped: true
     })
   }
 
+  /* 
+  @params
+
+  */
   nextWord = () => {
-    if (this.state.counter !== this.state.langData.length) {
-      let word = this.state.langData[this.state.counter + 1].word
-      fetchImg(word).then(data => {
-        console.log(data)
-        let counter = this.state.counter + 1
-        console.log(this.state.langData[counter])
-        this.setState({
-          counter,
-          word,
-          imgURL: data.hits[0] ? data.hits[0].largeImageURL : null,
-          translation: this.state.langData[counter].translation
-        })
-      })
-    } else {
-      let word = this.state.langData[0]
-      fetchImg(word).then(data => {
-        this.setState(
-          {
-            counter: 0,
-            word,
-            imgURL: data.hits[0].largeImageURL
-          },
-          () => fetchImg(this.state.word)
-        )
-      })
-    }
+    this.setState(
+      {
+        picFlipped: false
+      },
+      () => {
+        if (this.state.counter !== this.state.langData.length - 1) {
+          let word = this.state.langData[this.state.counter + 1].word
+          fetchImg(word).then(data => {
+            let counter = this.state.counter + 1
+            this.setState({
+              counter,
+              word,
+              imgURL: data.hits[0] ? data.hits[0].largeImageURL : null,
+              translation: this.state.langData[counter].translation
+            })
+          })
+        } else {
+          let word = this.state.langData[0].word
+          fetchImg(word).then(data => {
+            this.setState(
+              {
+                counter: 0,
+                word,
+                imgURL: data.hits[0].largeImageURL
+              },
+              () => fetchImg(this.state.word)
+            )
+          })
+        }
+      }
+    )
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h2>Recall {this.state.recallRate}%</h2>
           {this.state.imgURL && !this.state.picFlipped ? (
             <img
               onClick={() => {
@@ -116,16 +106,28 @@ class App extends React.PureComponent {
               src={this.state.imgURL}
               alt="word"
             />
-          ) : this.state.picFlipped ? (
-            <div>
-              <div className="noPic">No picture</div>
+          ) : !this.state.imgURL && !this.state.picFlipped ? (
+            <div
+              className="reveal"
+              onClick={() => {
+                this.reveal()
+              }}
+            >
+              No picture
             </div>
           ) : (
             <div>
               <h1>{this.state.translation}</h1>
               <div>
-                <button className="recallBtn">100% recall</button>
-                <button className="recallBtn btn-50">50% recall</button>
+                <button className="recallBtn" onClick={this.handleRecall100}>
+                  100% recall
+                </button>
+                <button
+                  className="recallBtn btn-50"
+                  onClick={this.handleRecall50}
+                >
+                  50% recall
+                </button>
                 <button className="recallBtn blank">blank</button>
               </div>
             </div>
